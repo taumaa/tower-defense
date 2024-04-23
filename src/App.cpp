e46514b2524132f528a3b660d80b5d38f51a2cb7
dsmtE <desmet.enguerrand@gmail.com>
2024-04-23 14:11:38 +0200

    ✨ Add App abstraction to have clean main file

---
+++ src/App.cpp
@@ -0,0 +1,71 @@
+#include "App.hpp"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+App::App() : _previousTime(0.0), _viewSize(2.0) {
+   // load what needs to be loaded here (for example textures)
+}
+
+void App::setup() {
+    // Set the clear color to a nice blue
+    glClearColor(0.0f, 0.0f, 0.4f, 1.0f);
+    glEnable(GL_DEPTH_TEST);
+}
+
+void App::update() {
+
+    const double currentTime { glfwGetTime() };
+    // const double elapsedTime { currentTime - _previousTime};
+    _previousTime = currentTime;
+    
+    render();
+}
+
+void App::render() {
+    // Clear the color and depth buffers of the frame buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_MODELVIEW);
+
+    // render exemple quad
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glBegin(GL_QUADS);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glVertex2f(0.5f, 0.5f);
+        glVertex2f(-0.5f, 0.5f);
+    glEnd();
+}
+
+void App::key_callback(int /*key*/, int /*scancode*/, int /*action*/, int /*mods*/) {
+}
+
+void App::mouse_button_callback(int /*button*/, int /*action*/, int /*mods*/) {
+}
+
+void App::scroll_callback(double /*xoffset*/, double /*yoffset*/) {
+}
+
+void App::cursor_position_callback(double /*xpos*/, double /*ypos*/) {
+}
+
+void App::size_callback(int width, int height) {
+    _width  = width;
+    _height = height;
+
+    // make sure the viewport matches the new window dimensions
+    glViewport(0, 0, _width, _height);
+
+    const float aspectRatio { _width / (float) _height };
+
+    // Change the projection matrix
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if(aspectRatio > 1.0f) {
+        glOrtho(-_viewSize / 2.0f * aspectRatio, _viewSize / 2.0f * aspectRatio, -_viewSize / 2.0f, _viewSize / 2.0f, -1.0f, 1.0f);
+    } else {
+        glOrtho(-_viewSize / 2.0f, _viewSize / 2.0f, -_viewSize / 2.0f / aspectRatio, _viewSize / 2.0f / aspectRatio, -1.0f, 1.0f);
+    }
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,15 @@
 
 #include <iostream>
 
+#include "App.hpp"
+
+namespace {
+    App& window_as_app(GLFWwindow* window)
+    {
+        return *static_cast<App*>(glfwGetWindowUserPointer(window));
+    }
+}
+
 // Optional: limit the frame rate
 constexpr double TARGET_TIME_FOR_FRAME { 1.0 / 60.0 };
 
@@ -26,6 +35,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #endif
+
     // Create window
     GLFWwindow* window { glfwCreateWindow(1280, 720, "Window", nullptr, nullptr) };
     if (!window) {
@@ -44,13 +54,42 @@ int main() {
         return -1;
     }
 
+    App app {};
+
+    glfwSetWindowUserPointer(window, &app);
+
+    glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+        window_as_app(window).key_callback(key, scancode, action, mods);
+    });
+    glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) {
+        window_as_app(window).mouse_button_callback(button, action, mods);
+    });
+    glfwSetScrollCallback(window, [](GLFWwindow* window, double xoffset, double yoffset) {
+        window_as_app(window).scroll_callback(xoffset, yoffset);
+    });
+    glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xpos, double ypos) {
+        window_as_app(window).cursor_position_callback(xpos, ypos);
+    });
+    glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+        window_as_app(window).size_callback(width, height);
+    });
+    
+    // Force calling the size_callback of the game to set the right viewport and projection matrix
+    {
+        int width, height;
+        glfwGetWindowSize(window, &width, &height);
+        app.size_callback(width, height);
+    }
+
+    app.setup();
+
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
 
         // Get time (in second) at loop beginning
 		double startTime { glfwGetTime() };
 
-        // Update and render here
+        app.update();
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
